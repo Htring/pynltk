ec3d16d93ad80292fa047e47f484d84dcd0ce627
Htring <1129501586@qq.com>
2023-01-14 09:25:47 +0800

    add ner __init__

---
+++ aitoolkit/ner/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :__init__.py.py
+# @Time      :2022/12/3 23:59
+# @Author    :juzipi
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
